9e145d68e6e25f8a70457cb1cc573990dc3ebc76
Guan Ce <ithinco@gmail.com>
2021-01-08 17:05:29 +0800

    Define RETCODEs

--- lib/callResult.go
+++ lib/callResult.go
@@ -4,6 +4,15 @@ import "time"
 
 type RetCode int
 
+const (
+	RET_CODE_SUCCESS RetCode = 0
+	RET_CODE_WARNING_TIMEOUT RetCode = 1001
+	RET_CODE_ERR_CALL RetCode = 2001
+	RET_CODE_ERR_RESPONSE RetCode = 2002
+	RET_CODE_ERR_CALLEE RetCode = 2003
+	RET_CODE_FATAL_CALL RetCode = 3001
+)
+
 type CallResult struct {
 	ID uint64
 	Req RawRequest
